be11e7575a77e8cf0a279d45463fc47d94ac614b
Mark Orlando Zeller <45102464+MaZe3D@users.noreply.github.com>
2022-03-28 22:20:49 +0200

    inserted Airlock-Skript

--- Airlock/Program.cs
+++ Airlock/Program.cs
@@ -22,62 +22,214 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+        AirLock[] airLockList = new AirLock[10];
+        DateTime time = DateTime.UtcNow;
 
         public Program()
         {
-            // The constructor, called only once every session and
-            // always before any other method is called. Use it to
-            // initialize your script. 
-            //     
-            // The constructor is optional and can be removed if not
-            // needed.
-            // 
-            // It's recommended to set Runtime.UpdateFrequency 
-            // here, which will allow your script to run itself without a 
-            // timer block.
-        }
-
-        public void Save()
-        {
-            // Called when the program needs to save its state. Use
-            // this method to save your state to the Storage field
-            // or some other means. 
-            // 
-            // This method is optional and can be removed if not
-            // needed.
+            Runtime.UpdateFrequency = UpdateFrequency.Update10;
+            for (int i = 0; i < airLockList.Length; i++)
+            {
+                airLockList[i] = new AirLock();
+            }
+            initAirLocks();
+            for (int i = 0; i < airLockList.Length; i++)
+            {
+                if (!airLockList[i].Valid)
+                {
+                    airLockList[i] = null;
+                }
+            }
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            // The main entry point of the script, invoked every time
-            // one of the programmable block's Run actions are invoked,
-            // or the script updates itself. The updateSource argument
-            // describes where the update came from. Be aware that the
-            // updateSource is a  bitfield  and might contain more than 
-            // one update type.
-            // 
-            // The method itself is required, but the arguments above
-            // can be removed if not needed.
+            time = DateTime.UtcNow;
+            Echo($"=== Airlock Manager v1.0 ===");
+            for (int i = 0; i < airLockList.Length; i++)
+            {
+                if (airLockList[i] != null)
+                {
+                    Echo($"AirLock No. {i}: {airLockList[i].Status}");
+                    airLockList[i].Manager();
+                }
+            }
+            Echo("");
+            Echo("=== Stistics ===");
+            Echo($"Stript Runtime: {(DateTime.UtcNow - time).TotalSeconds}ms");
         }
+
+        void initAirLocks()
+        {
+            List<IMyTerminalBlock> allDoors = new List<IMyTerminalBlock>();
+            GridTerminalSystem.GetBlocksOfType<IMyDoor>(allDoors);
+
+            foreach (IMyDoor door in allDoors)
+            {
+                string[] parameter;
+                int doorNumber;
+                int airLockNumber;
+                double delay;
+                if (door.CustomName.StartsWith("AirLock_"))
+                {
+                    parameter = door.CustomName.Split('_', ' ');
+                    if (parameter.Length >= 3)
+                    {
+                        doorNumber = Convert.ToInt32(parameter[2]);
+                        airLockNumber = Convert.ToInt32(parameter[1]);
+                        switch (doorNumber)
+                        {
+                            default:
+
+                            case 1:
+                                Echo("Case 1");
+                                airLockList[airLockNumber].door1 = door;
+                                break;
+                            case 2:
+                                airLockList[airLockNumber].door2 = door;
+                                Echo("Case 2");
+                                break;
+                        }
+
+                        if (parameter.Length >= 4)
+                        {
+                            try
+                            {
+                                delay = Convert.ToDouble(parameter[3]);
+                                airLockList[airLockNumber].delayInMiliseconds = delay;
+                            }
+                            catch (Exception)
+                            {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public class AirLock
+    {
+        public double delayInMiliseconds = 4000;
+        public AirLockStatus Status = AirLockStatus.Idle;
+        public IMyDoor door1;
+        public IMyDoor door2;
+        int doorOpened = 0;
+
+        DateTime t1 = DateTime.Now;
+
+        public AirLock()
+        {
+
+        }
+
+        public void Manager()
+        {
+            if (!Valid)
+                return;
+
+            int ALSreturn = ALS();
+
+            switch (Status)
+            {
+                case AirLockStatus.Idle:
+                    if ((doorOpened = ALSreturn) != 0)
+                    {
+                        Status = AirLockStatus.In;
+                        t1 = DateTime.Now;
+                    }
+                    break;
+                case AirLockStatus.In:
+                    if ((DateTime.Now - t1).TotalMilliseconds >= delayInMiliseconds)
+                    {
+                        Status = AirLockStatus.closeDoor;
+                    }
+                    break;
+                case AirLockStatus.closeDoor:
+                    closeDoors();
+                    if (ALSreturn == 0)
+                    {
+                        Status = AirLockStatus.Out;
+                        t1 = DateTime.Now;
+                    }
+                    break;
+                case AirLockStatus.Out:
+                    if (doorOpened == 1)
+                        door2.OpenDoor();
+                    else if (doorOpened == 2)
+                        door1.OpenDoor();
+
+                    if ((DateTime.Now - t1).TotalMilliseconds >= delayInMiliseconds)
+                    {
+                        Status = AirLockStatus.closeDoor2;
+                    }
+                    break;
+                case AirLockStatus.closeDoor2:
+                    closeDoors();
+                    if (ALSreturn == 0)
+                    {
+                        Status = AirLockStatus.Idle;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        public int ALS()
+        {
+            if (door1.Status == DoorStatus.Opening || door1.Status == DoorStatus.Open || door1.Status == DoorStatus.Closing)
+            {
+                door2.Enabled = false;
+                return 1;
+            }
+            else if (door1.Status == DoorStatus.Closed)
+            {
+                door2.Enabled = true;
+            }
+
+            if (door2.Status == DoorStatus.Opening || door2.Status == DoorStatus.Open || door2.Status == DoorStatus.Closing)
+            {
+                door1.Enabled = false;
+                return 2;
+            }
+            else if (door2.Status == DoorStatus.Closed)
+            {
+                door1.Enabled = true;
+            }
+
+            return 0;
+        }
+
+        public bool Valid
+        {
+            get
+            {
+                if ((door1 != null) && (door2 != null))
+                    return true;
+                else return false;
+            }
+        }
+
+        public void closeDoors()
+        {
+            if (Valid)
+            {
+                door1.CloseDoor();
+                door2.CloseDoor();
+            }
+        }
+    }
+    public enum AirLockStatus
+    {
+        Init = 0,
+        Idle = 1,
+        In = 2,
+        closeDoor = 3,
+        Out = 4,
+        closeDoor2 = 5,
+
     }
 }
